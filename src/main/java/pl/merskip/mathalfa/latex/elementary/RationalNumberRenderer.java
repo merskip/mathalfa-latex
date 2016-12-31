20d54a99f9d37c0b2056e93eaa3eb6f1ea27dad6
Piotr Merski <merskip@gmail.com>
2016-12-31 15:22:40 +0100

    Jeśli licznik jest ujemny, to znak będzie renderowany przed całym ułamkiem

--- src/main/java/pl/merskip/mathalfa/latex/elementary/RationalNumberRenderer.java
+++ src/main/java/pl/merskip/mathalfa/latex/elementary/RationalNumberRenderer.java
@@ -4,18 +4,21 @@ import pl.merskip.mathalfa.base.elementary.RationalNumber;
 import pl.merskip.mathalfa.latex.core.RendererRegister;
 import pl.merskip.mathalfa.latex.core.SymbolRenderer;
 
+import static java.lang.Math.abs;
+
 public class RationalNumberRenderer implements SymbolRenderer<RationalNumber> {
     
     @Override
     public String renderSymbol(RendererRegister register, RationalNumber symbol) {
-        String numerator = String.valueOf(symbol.getNumerator());
+        String sign = symbol.getNumerator() < 0 ? "-" : "";
+        String numerator = String.valueOf(abs(symbol.getNumerator()));
         String denominator = String.valueOf(symbol.getDenominator());
         
         if (symbol.getDenominator() == 1) {
-            return numerator;
+            return sign + numerator;
         }
         else {
-            return String.format("\\cfrac{%s}{%s}", numerator, denominator);
+            return String.format("%s\\cfrac{%s}{%s}", sign, numerator, denominator);
         }
     }
 }
