7b53c7eb8cf1bdd4d4748ecb54b72aa68c32ff75
Piotr Merski <merskip@gmail.com>
2016-12-31 14:23:25 +0100

    Dodawanie nawiasów do pierwszego argumentu przy operatorach tylko gdy jest to naprawdę potrzebne

--- src/main/java/pl/merskip/mathalfa/latex/elementary/OperatorRenderer.java
+++ src/main/java/pl/merskip/mathalfa/latex/elementary/OperatorRenderer.java
@@ -3,6 +3,7 @@ package pl.merskip.mathalfa.latex.elementary;
 import pl.merskip.mathalfa.base.core.Operator;
 import pl.merskip.mathalfa.base.core.Symbol;
 import pl.merskip.mathalfa.base.elementary.NumberAddition;
+import pl.merskip.mathalfa.base.elementary.NumberSubtraction;
 import pl.merskip.mathalfa.base.elementary.RationalNumber;
 import pl.merskip.mathalfa.latex.core.RendererRegister;
 
@@ -21,21 +22,43 @@ public abstract class OperatorRenderer {
         String firstArgLatex = register.renderSymbol(firstArgument);
         String secondArgLatex = register.renderSymbol(secondArgument);
         
-        if (symbolNeedsParentheses(firstArgument)) {
+        if (firstArgumentNeedsParentheses(firstArgument)) {
             firstArgLatex = String.format("\\left( %s \\right)", firstArgLatex);
         }
-        if (symbolNeedsParentheses(secondArgument)) {
+        if (secondArgumentNeedsParentheses(secondArgument)) {
             secondArgLatex = String.format("\\left( %s \\right)", secondArgLatex);
         }
         
         return firstArgLatex + " " + operatorChar + " " + secondArgLatex;
     }
     
-    private boolean symbolNeedsParentheses(Symbol symbol) {
-        if (symbol instanceof RationalNumber) {
-            RationalNumber firstArgument = (RationalNumber) symbol;
-            return firstArgument.getDenominator() != 1;
+    @SuppressWarnings("RedundantIfStatement")
+    private boolean firstArgumentNeedsParentheses(Symbol firstArgument) {
+        if (symbolIsIntegerNumber(firstArgument)
+                || firstArgument instanceof NumberAddition
+                || firstArgument instanceof NumberSubtraction ) {
+            return false;
         }
-        return true;
+        else {
+            return true;
+        }
+    }
+    
+    @SuppressWarnings("RedundantIfStatement")
+    private boolean secondArgumentNeedsParentheses(Symbol firstArgument) {
+        if (symbolIsIntegerNumber(firstArgument)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    private boolean symbolIsIntegerNumber(Symbol symbol) {
+        if (symbol instanceof RationalNumber) {
+            RationalNumber number = (RationalNumber) symbol;
+            return number.getDenominator() == 1;
+        }
+        return false;
     }
 }
