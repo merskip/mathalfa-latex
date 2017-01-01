5ac1790ab58f2743e36f2ee7aeca88fbad3c0088
Piotr Merski <merskip@gmail.com>
2017-01-01 21:36:45 +0100

    Wyświetlanie potęg postaci 1/n jako pierwiastki

--- src/main/java/pl/merskip/mathalfa/latex/elementary/NumberExponentiationRenderer.java
+++ src/main/java/pl/merskip/mathalfa/latex/elementary/NumberExponentiationRenderer.java
@@ -6,12 +6,26 @@ import pl.merskip.mathalfa.base.elementary.RationalNumber;
 import pl.merskip.mathalfa.latex.core.RendererRegister;
 import pl.merskip.mathalfa.latex.core.SymbolRenderer;
 
+import java.math.BigInteger;
+
 public class NumberExponentiationRenderer implements SymbolRenderer<NumberExponentiation> {
     
     @Override
     public String renderSymbol(RendererRegister register, NumberExponentiation symbol) {
         String baseLatex = register.renderSymbol(symbol.getBase());
         String powerLatex = register.renderSymbol(symbol.getPower());
+        if (symbol.getPower() instanceof RationalNumber) {
+            RationalNumber power = (RationalNumber) symbol.getPower();
+            if (power.inverted().isInteger()) {
+                if (power.getDenominator().compareTo(BigInteger.valueOf(2)) == 0) {
+                    return String.format("\\sqrt{%s}", baseLatex);
+                }
+                else {
+                    return String.format("\\sqrt[%s]{%s}",
+                            register.renderSymbol(power.inverted()), baseLatex);
+                }
+            }
+        }
         if (baseNeedsParentheses(symbol.getBase())) {
             return String.format("\\left( %s \\right)^{ %s }", baseLatex, powerLatex);
         }
